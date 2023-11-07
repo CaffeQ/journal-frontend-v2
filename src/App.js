3834163ca3b3dd9f1839a8b503ff47f0628fde5b
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2023-11-07 16:53:16 +0100

    added dockerfile and get patient

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setstate] = useState(null);
+  useEffect(()=>{
+    fetch('http://localhost:8080/patient')
+      .then((response) => response.json())
+      .then((data)=> {
+        console.log(data);
+        setstate(data)
+      })
+      .catch((err)=>{
+        console.log(err.message);
+      })
+  },[])
+
+  if(state != null)
+    return(
+      <html>
+        <body>
+          <h1>{state.id}</h1>
+          <p>{state.name}</p>
+        </body>
+      </html>
+    )
+  else
+  return(
+    <html>
+      <body>
+        <p>loading...</p>
+      </body>
+    </html>
+  )
+
 }
 
 export default App;
