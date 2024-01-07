52a8164583138fee7da548b5dbe68606af01e36f
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2024-01-07 14:32:39 +0100

    added with credentials

--- src/services/PatientService.js
+++ src/services/PatientService.js
@@ -7,23 +7,23 @@ class PatientService{
     getPatients(){
         console.log("URL="+process.env.REACT_APP_PATIENT_SERVICE_URL)
 
-        return axios.get(BASE_URL + "/journal/patients");
+        return axios.get(BASE_URL + "/journal/patients",{ withCredentials: true });
     }
     getMyPatientDetails(){
         return axios.get(BASE_URL+ "/journal/patient/details", { withCredentials: true });
     }
     getPatientDetails(id){
-        return axios.get(BASE_URL+"/journal/patient?id="+id);
+        return axios.get(BASE_URL+"/journal/patient?id="+id,{ withCredentials: true });
     }
     postObservation(observation){
-        return axios.post(BASE_URL+"/journal/observation",observation)
+        return axios.post(BASE_URL+"/journal/observation",observation,{ withCredentials: true })
     }
     postEncounter(encounter){
-        return axios.post(BASE_URL+"/journal/encounter",encounter); // ändra adress
+        return axios.post(BASE_URL+"/journal/encounter",encounter,{ withCredentials: true }); // ändra adress
     }
     postCondition(id, diagnosis){
         const url = `${BASE_URL}/journal/diagnosis?patientID=${id}&diagnosis=${encodeURIComponent(diagnosis)}`
-        return axios.post(url); // ändra adress
+        return axios.post(url,{ withCredentials: true }); // ändra adress
     }
     /*
     postCondition(id, diagnosis) {
